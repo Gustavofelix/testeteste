9e691ae0eb1f24a7d78130197476059d1f9de6e9
Gustavofelix <32615577+Gustavofelix@users.noreply.github.com>
2017-10-09 23:08:53 +0000

    Update config.default.js

--- recipes/speech_to_text/config.default.js
+++ recipes/speech_to_text/config.default.js
@@ -12,6 +12,6 @@ exports.credentials = {};
 // Watson Speech to Text
 // https://www.ibm.com/watson/developercloud/speech-to-text.html
 exports.credentials.speech_to_text = {
-    password: '',
-    username: ''
+    password: 'v7VKpBUjkmKz',
+    username: '79d6fc92-ed8a-454f-b9ac-dc24677c3878'
 };
